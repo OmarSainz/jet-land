2a55a26cee7af609b1a7067eab77c96f17627763
Jose Alvarez <jose@Joses-MacBook-Pro.local>
2014-07-29 12:07:32 -0500

    date and tuit url added

--- public/app/assets/js/main.js
+++ public/app/assets/js/main.js
@@ -1,7 +1,12 @@
 var main = function () {
     window.client = io.connect(window.location.href);
     client.on('tweet', function (data) {        
-        console.log("Llegó un twit: " + data.name + " " + data.text + " " + data.image);
+        console.log("Llegó un twit: ");
+        console.log("Usuario: " + data.name);
+        console.log("Lo que dice el tuit: " + data.text);
+        console.log("Avatar del usuario: " + data.image);
+        console.log("Fecha: " + data.date);
+        console.log("Url: " + data.url);
     });
 }
 
