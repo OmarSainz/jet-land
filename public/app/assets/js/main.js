14fb4e0011e871c2cc33fefbdb8bddf32d67b766
Omar Sainz <SQL_Slammer.exe@live.de>
2014-07-29 09:43:08 -0600

    Update main and index

--- public/app/assets/js/main.js
+++ public/app/assets/js/main.js
@@ -2,6 +2,7 @@ var main = function () {
     window.client = io.connect(window.location.href);
     client.on('tweet', function (data) {        
         console.log("Llegó un twit: " + data.name + " " + data.text + " " + data.image);
+        $('.tweets').append('<img src=' + data.image + '/>' + '<span>' + data.text + '</span>');
     });
 }
 
